102a2c059be3320becbc74bd41f82acb4c53fafc
Alan Lansdowne <daimyou@rounin.co.uk>
2020-01-11 09:23:37 +0000

    Update javascript.php

--- javascript.php
+++ javascript.php
@@ -1,23 +1,23 @@
 <?php
 
-  //=================//
+  //*****************//
  // ERROR REPORTING //
-//=================//
+//*****************//
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 
-  //==================//
+  //******************//
  // GZIP COMPRESSION //
-//==================//
+//******************//
 
 ob_start ("ob_gzhandler");
 
 
-  //===============//
+  //***************//
  // BUILD HEADERS //
-//===============//
+//***************//
 
 header("Content-type: application/javascript; charset: UTF-8");
 header("Cache-Control: must-revalidate");
@@ -26,9 +26,9 @@ $Expires_Header = "Expires: ".gmdate("D, d M Y H:i:s", time() + $offset)." GMT";
 header($Expires_Header);
 
 
-  //================//
+  //****************//
  // ADD JAVASCRIPT //
-//================//
+//****************//
 
 echo 'console.log(\'This is an example of a Javascript file\');'."\n";
 echo 'console.log(\'made even more dynamic using PHP\; in which it is possible\');'."\n";
